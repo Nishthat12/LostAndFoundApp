5336d70b1309fc7fd9e06b133c3b6d9311fa8843
nishthat <nishtha.taktewale12@gmail.com>
2022-10-26 22:56:53 +0530

    Connected authentication email and password to firebase

--- app/src/main/java/com/example/lostandfoundappcs230/MainActivity.kt
+++ app/src/main/java/com/example/lostandfoundappcs230/MainActivity.kt
@@ -3,47 +3,64 @@ package com.example.lostandfoundappcs230
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.Window
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lostandfoundappcs230.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-
-
-    // define the global variable
-    // Add button Move to Activity
+    private lateinit var  binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var Signup_btn: Button
     private lateinit var Signin_btn: Button
 
-
     @SuppressLint("CutPasteId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)//will hide the title
+        requestWindowFeature(Window.FEATURE_NO_TITLE) //will hide the title
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
         supportActionBar?.hide() //hide the title bar
 
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
         // by ID we can use each component which id is assign in xml file
         // use findViewById() to get the Button
         Signup_btn = findViewById(R.id.signup_btn)
 
-        // In question1 get the TextView use by findViewById()
-        // In TextView set question Answer for message
-
         // Add_button add clicklistener
         Signup_btn.setOnClickListener {
-            // Intents are objects of the android.content.Intent type. Your code can send them to the Android system defining
-            // the components you are targeting. Intent to start an activity called SignUpActivity with the following code.
             val intent = Intent(this, SignUpActivity::class.java)
             // start the activity connect to the specified class
             startActivity(intent)
+            finish()
         }
         Signin_btn = findViewById(R.id.signin_btn)
-        Signin_btn.setOnClickListener{
-            val intent = Intent(this, HomePageActivity::class.java)
-            startActivity(intent)
-        }
+        binding.signinBtn.setOnClickListener {
+            val email = binding.etLoginEmail.text.toString()
+            val password = binding.etLoginPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() ) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val intent = Intent(this, HomePageActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            } else {
+                Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show()
+            } }
 
     }
 }
--- app/src/main/java/com/example/lostandfoundappcs230/SignUpActivity.kt
+++ app/src/main/java/com/example/lostandfoundappcs230/SignUpActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lostandfoundappcs230.databinding.ActivityMainBinding
 import com.example.lostandfoundappcs230.databinding.ActivitySignupactivityBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -16,51 +17,58 @@ class SignUpActivity : AppCompatActivity() {
 
     // define the global variable
     // Add button Move to next Activity and previous Activity
-    private lateinit var binding: ActivitySignupactivityBinding
-    private lateinit var database : DatabaseReference
+    private lateinit var binding: ActivitySignupactivityBinding //binding
+    private lateinit var database: DatabaseReference
     private lateinit var signup_btn: Button
+    private lateinit var firebaseAuth: FirebaseAuth
 //    private lateinit var previous_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        setContentView(R.layout.activity_signupactivity)
+        supportActionBar?.hide() //hide the title bar
+
         binding = ActivitySignupactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_signupactivity)
-        supportActionBar?.hide(); //hide the title bar
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btRegister.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            val repassword = binding.etRepeatPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && repassword.isNotEmpty()) {
+                if (password == repassword) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         // by ID we can use each component which id is assign in xml
         // file use findViewById() to get the both Button and textview
         signup_btn = findViewById(R.id.bt_register)
 
-        signup_btn.setOnClickListener {
-            // Intents are objects of the android.content.Intent type. Your code can send them to the Android system defining
-            // the components you are targeting. Intent to start an activity called ThirdActivity with the following code.
-            val intent = Intent(this, HomePageActivity::class.java)
-            // start the activity connect to the specified class
-            startActivity(intent)
-        }
-        binding.btRegister.setOnClickListener{
-            val Name = binding.name.text.toString()
-            val RollNumber = binding.etRollno.text.toString()
-            val Email = binding.etEmail.text.toString()
-            val Password = binding.etPassword.text.toString()
-            val ContactNumber = binding.etNumber.text.toString()
-
-            database = FirebaseDatabase.getInstance().getReference("User")
-
-            val User = User(Name,RollNumber,Email, Password,ContactNumber)
-            database.child(Email).setValue(User).addOnSuccessListener {
-                binding.name.text.clear()
-                binding.etRollno.text.clear()
-                binding.etEmail.text.clear()
-                binding.etPassword.text.clear()
-                binding.etNumber.text.clear()
-
-                Toast.makeText(this, "Succesfully Saved", Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener{
-                Toast.makeText(this, "Failed",Toast.LENGTH_SHORT).show()
-            }
-        }
+//        signup_btn.setOnClickListener {
+//            // Intents are objects of the android.content.Intent type. Your code can send them to the Android system defining
+//            // the components you are targeting. Intent to start an activity called ThirdActivity with the following code.
+//            val intent = Intent(this, HomePageActivity::class.java)
+//            // start the activity connect to the specified class
+//            startActivity(intent)
+//        }
 //        // Add_button add clicklistener
 //        previous_button.setOnClickListener {
 //            // Intents are objects of the android.content.Intent type. Your code can send them to the Android system defining
